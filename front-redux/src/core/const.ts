7ba170270f4766066734d8c542624d886f8fe70e
Braulio Diez Botella <braulio.diez@gmail.com>
2020-09-03 12:49:40 +0200

    updated to ws

--- front-redux/src/core/const.ts
+++ front-redux/src/core/const.ts
@@ -1,6 +1,6 @@
 // TODO: Add env variables
 export const baseApiUrl = 'http://localhost:8081';
-export const baseSocketUrl = 'http://localhost:3000';
+export const baseSocketUrl = 'ws://localhost:3000';
 
 // TODO we could extract inspecting current URL
 export const appBaseUrl = 'http://localhost:8080';
