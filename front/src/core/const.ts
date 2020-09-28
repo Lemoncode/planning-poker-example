a7758afe45a0ff5b762b0a4700ad1cecfb47852a
Braulio Diez Botella <braulio.diez@gmail.com>
2020-09-28 20:27:24 +0200

    temporary fix

--- front/src/core/const.ts
+++ front/src/core/const.ts
@@ -3,7 +3,7 @@ export const baseApiUrl = 'http://localhost:8081';
 export const baseSocketUrl = 'http://localhost:3000';
 
 // TODO we could extract inspecting current URL
-export const appBaseUrl = 'http://localhost:8080';
+export const appBaseUrl = 'http://localhost:8080/app.html';
 
 export const SocketInputMessageTypes = {
   CONNECTION_ESTABLISHED_PLAYER: 'CONNECTION_ESTABLISHED_PLAYER',
