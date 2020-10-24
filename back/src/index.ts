eddb21138417877dc639bcf651841eac1715d753
Luis del Amo <luis@salonkee.lu>
2020-10-25 01:35:18 +0200

    ref:#35 removed throw error

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,13 +2,15 @@ import 'regenerator-runtime/runtime';
 import * as fs from 'fs';
 import * as path from 'path';
 import { config } from 'dotenv';
+import colors from 'colors';
 try {
   fs.readFileSync(path.join(__dirname, '..', '.env'), 'utf-8');
 } catch (e) {
-  const message = `.env file doesn't exist`;
+  const message = `${colors.yellow('WARNING:')} ${colors.cyan(
+    '".env"'
+  )} file doesn't exist in:`;
   console.error(message);
   console.error(e.path);
-  throw message;
 }
 
 config();
