de79ce16a0189d3778277f292362d6e2c5d0463e
Luis del Amo <luis@salonkee.lu>
2020-10-27 22:27:32 +0100

    ref:#35 reorder file

--- back/src/app.ts
+++ back/src/app.ts
@@ -8,11 +8,6 @@ import colors from 'colors';
 
 const app = createApp();
 
-const http = require('http').Server(app);
-// set up socket.io and bind it to our
-// http server.
-const io: SocketIOClient.Socket = require('socket.io')(http);
-
 app.use(envConstants.apiUrl, roomApi);
 
 app.listen(envConstants.PORT, () => {
@@ -23,11 +18,13 @@ app.listen(envConstants.PORT, () => {
   );
 });
 
+const http = require('http').Server(app);
+// set up socket.io and bind it to our
+// http server.
+const io: SocketIOClient.Socket = require('socket.io')(http);
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
-io.on('connection', async (socket: Socket) => {
-  await sockets(socket, io);
-});
+io.on('connection', async (socket: Socket) => await sockets(socket, io));
 
 http.listen(3000, async () => {
   if (!envConstants.isApiMock && envConstants.MONGODB_URI) {
