11628c15cff1560f861f86a41c301588586a5f5d
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-01 16:05:13 +0100

    Footer link component spec completed

---
+++ front/src/common-app/components/footer/footer.component.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Footer } from './footer.component';
+
+describe('Footer component spec', () => {
+  it('', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+});
