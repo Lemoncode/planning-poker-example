b7e683deca4fa7684e2e1250c4976ae16aefcdfb
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-11 19:23:43 +0100

    problem with stab unsolved yet

--- front/src/common/components/snackbar/snackbar.hook.spec.ts
+++ front/src/common/components/snackbar/snackbar.hook.spec.ts
@@ -1,7 +1,19 @@
-import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { useSnackbarContext } from './snackbar.hook';
+import * as SnackbarContext from './snackbar.context';
 
 describe('Snackbar hook spec', () => {
-  it('dummy', () => {});
+  it('"showMessage" should be a funcion by default', () => {
+    // Arrange
+
+    // Act
+    const { result } = renderHook(() => useSnackbarContext());
+
+    jest
+      .spyOn(SnackbarContext, 'SnackbarProvider')
+      .mockImplementation(() => ({ setOptions: jest.fn }));
+
+    // Assert
+    expect(result.current.showMessage).toEqual(expect.any(Function));
+  });
 });
