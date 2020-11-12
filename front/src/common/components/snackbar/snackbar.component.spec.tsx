4bef2387e6a260526a85384f3ccabb0011997baa
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-12 12:09:09 +0100

    snackbar component in process

--- front/src/common/components/snackbar/snackbar.component.spec.tsx
+++ front/src/common/components/snackbar/snackbar.component.spec.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { SnackbarProvider } from './snackbar.context';
+import { SnackbarProvider, SnackbarContext } from './snackbar.context';
 import { SnackbarComponent } from './snackbar.component';
 
 describe('Snackbar component specs', () => {
   it('', () => {
     // Arrange
+    const TestComponent: React.FC = () => {
+      const { setOpen } = React.useContext(SnackbarContext);
+      React.useEffect(() => setOpen(true), []);
+      return <></>;
+    };
 
     // Act
     render(
       <SnackbarProvider>
+        <TestComponent />
         <SnackbarComponent />
       </SnackbarProvider>
     );
 
     // Assert
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 });
