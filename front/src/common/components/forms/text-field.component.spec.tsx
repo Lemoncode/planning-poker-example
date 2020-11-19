8b2361e21e9c6f613f1ad67450cd5f3c9ddc110f
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-19 20:00:38 +0100

    test not working

--- front/src/common/components/forms/text-field.component.spec.tsx
+++ front/src/common/components/forms/text-field.component.spec.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { useField } from 'formik';
-import * as Formik from 'formik';
+import { Formik, useField } from 'formik';
 import { render, screen } from '@testing-library/react';
-import { renderHook } from '@testing-library/react-hooks';
 import { TextFieldComponent } from './text-field.component';
 
 describe('textfield component specs', () => {
+  const renderWithFormik = (component, initialValues) => ({
+    ...render(
+      <Formik initialValues={initialValues} onSubmit={console.log}>
+        {() => component}
+      </Formik>
+    ),
+  });
+
   it('', () => {
     // Arrange
 
     // Act
-    // const {result} = renderHook(() => useField('test name'));
-    // jest.spyOn(Formik, 'useField').mockReturnValue({});
 
-    render(<TextFieldComponent />);
+    renderWithFormik(<TextFieldComponent />, { name: 'test name' });
 
     const textarea = screen.getByRole('textbox');
 
