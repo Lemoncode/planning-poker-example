c3ad594e05a23f2d0584928a27c68c617c77c6af
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-19 17:20:33 +0100

    latest changes

--- front/src/common/components/snackbar/snackbar.component.spec.tsx
+++ front/src/common/components/snackbar/snackbar.component.spec.tsx
@@ -4,38 +4,36 @@ import { SnackbarProvider, SnackbarContext } from './snackbar.context';
 import { SnackbarComponent } from './snackbar.component';
 
 describe('Snackbar component specs', () => {
+  // it('', () => {
+  //   // Arrange
+  //   const TestComponent: React.FC = () => {
+  //     const { setOpen } = React.useContext(SnackbarContext);
+  //     React.useEffect(() => setOpen(true), []);
+  //     return <></>;
+  //   };
+
+  //   // Act
+  //   render(
+  //     <SnackbarProvider>
+  //       <TestComponent />
+  //       <SnackbarComponent />
+  //     </SnackbarProvider>
+  //   );
+
+  //   // Assert
+  //   expect(screen.getByRole('button')).toBeInTheDocument();
+  // });
+
   it('', () => {
-    // Arrange
-    const TestComponent: React.FC = () => {
-      const { setOpen } = React.useContext(SnackbarContext);
-      React.useEffect(() => setOpen(true), []);
-      return <></>;
-    };
-
-    // Act
-    render(
-      <SnackbarProvider>
-        <TestComponent />
-        <SnackbarComponent />
-      </SnackbarProvider>
-    );
-
-    // Assert
-    expect(screen.getByRole('button')).toBeInTheDocument();
-  });
-
-  xit('', () => {
     // Arrange
     jest.spyOn(React, 'useContext').mockReturnValue({
       open: true,
+      onClose: jest.fn(),
+      options: { variant: 'success', message: 'text message' },
     });
 
     // Act
-    render(
-      <SnackbarProvider>
-        <SnackbarComponent />
-      </SnackbarProvider>
-    );
+    render(<SnackbarComponent />);
 
     // Assert
     expect(screen.getByRole('button')).toBeInTheDocument();
--- front/src/common/components/snackbar/snackbar.hook.spec.tsx
+++ front/src/common/components/snackbar/snackbar.hook.spec.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { useSnackbarContext } from './snackbar.hook';
 import { SnackbarProvider } from './snackbar.context';
+import { act } from '@testing-library/react';
 
 describe('Snackbar hook spec', () => {
   it('"showMessage" should be a funcion by default', () => {
@@ -17,5 +18,30 @@ describe('Snackbar hook spec', () => {
 
     // Assert
     expect(result.current.showMessage).toEqual(expect.any(Function));
+  });
+
+  it('"showMessage" should be a funcion by default', () => {
+    // Arrange
+    const provider: React.FC = props => (
+      <SnackbarProvider>{props.children}</SnackbarProvider>
+    );
+    const setOptions = jest.fn();
+    const setOpen = jest.fn();
+
+    jest.spyOn(React, 'useContext').mockReturnValue({ setOptions, setOpen });
+
+    // Act
+    const { result } = renderHook(() => useSnackbarContext(), {
+      wrapper: provider,
+    });
+
+    result.current.showMessage('Test message', 'success');
+
+    // Assert
+    expect(setOptions).toHaveBeenCalledWith({
+      message: 'Test message',
+      variant: 'success',
+    });
+    expect(setOpen).toHaveBeenCalledWith(true);
   });
 });
