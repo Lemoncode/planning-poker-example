0437ba171eef084b01f8e9341c2a27a7b8eec0c2
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-19 12:33:20 +0100

    Titles updated

--- front/src/scenes/player.scene.tsx
+++ front/src/scenes/player.scene.tsx
@@ -3,6 +3,9 @@ import { PlayerContainer } from 'pods/player';
 import { AppLayout } from 'layout';
 
 export const PlayerScene = () => {
+  React.useEffect(() => {
+    document.title = `Player Page - T-Shirt Planning Poker - Lemoncode`;
+  }, []);
   return (
     <AppLayout>
       <PlayerContainer />
