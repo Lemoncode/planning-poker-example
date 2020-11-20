78583e4092c284dc98a8cf61bfa854b052ffc869
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-11-20 10:00:56 +0100

    test working

--- front/src/core/auth.context.spec.tsx
+++ front/src/core/auth.context.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { AuthContext, AuthProvider } from './auth.context';
 
 describe('Auth context specs', () => {
@@ -23,11 +24,13 @@ describe('Auth context specs', () => {
     expect(heading).toBeInTheDocument();
   });
 
-  it('"nickname should be an empty string by default"', () => {
+  it('"setNickname" should be an empty string by default', () => {
     // Arrange
     const TestComponent: React.FC = () => {
-      const { nickname } = React.useContext(AuthContext);
-      return <>{nickname === '' && <h1>Test</h1>}</>;
+      const { setNickname } = React.useContext(AuthContext);
+      const isFunction = fn =>
+        fn && {}.toString.call(fn) === '[object Function]';
+      return <>{isFunction(setNickname) && <h1>Test</h1>}</>;
     };
 
     // Act
@@ -38,6 +41,61 @@ describe('Auth context specs', () => {
     );
 
     const heading: HTMLElement = screen.getByRole('heading', { name: 'Test' });
+
+    // Assert
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('"nickname" should be an empty string by default', () => {
+    // Arrange
+    const TestComponent: React.FC = () => {
+      const { setNickname } = React.useContext(AuthContext);
+      const isFunction = fn =>
+        fn && {}.toString.call(fn) === '[object Function]';
+      return <>{isFunction(setNickname) && <h1>Test</h1>}</>;
+    };
+
+    // Act
+    render(
+      <AuthProvider>
+        <TestComponent />
+      </AuthProvider>
+    );
+
+    const heading: HTMLElement = screen.getByRole('heading', { name: 'Test' });
+
+    // Assert
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('"setNickname" should change value of "nickname"', () => {
+    // Arrange
+    const TestComponent: React.FC = () => {
+      const { setNickname, nickname } = React.useContext(AuthContext);
+      return (
+        <>
+          <button onClick={() => setNickname('New Test Value')}></button>
+          {nickname === 'New Test Value' && <h1>Test</h1>}
+        </>
+      );
+    };
+
+    // Act
+    render(
+      <AuthProvider>
+        <TestComponent />
+      </AuthProvider>
+    );
+
+    const button: HTMLElement = screen.getByRole('button');
+
+    act(() => {
+      userEvent.click(button);
+    });
+
+    const heading: HTMLElement = screen.getByRole('heading', {
+      name: 'Test',
+    });
 
     // Assert
     expect(heading).toBeInTheDocument();
