44951d3fa41512998a331d5c3e9b87969f4d9f37
daniel.sanchez <daniel.sanchez@lemoncode.net>
2022-09-20 13:58:21 +0200

    remove redirect

--- back/src/core/servers/express.server.ts
+++ back/src/core/servers/express.server.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { envConstants } from 'core/constants';
 import { corsOptions } from './cors';
-import { redirectHttpsMiddleware } from './middlewares';
 
 export const createApp = () => {
   const app = express();
@@ -11,8 +9,5 @@ export const createApp = () => {
   app.use(bodyParser.json());
   app.use(corsOptions);
 
-  if (envConstants.isProduction) {
-    app.use(redirectHttpsMiddleware);
-  }
   return app;
 };
